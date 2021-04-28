c87bbd8c45db99177d3b2ee0b2f082501f4758d0
Jonas Hübotter <jonas.huebotter@gmail.com>
2021-04-28 22:31:39 +0200

    [#54] Deterministic Online Algorithm (Bansal)

--- soco/src/algorithms/bansal.rs
+++ soco/src/algorithms/bansal.rs
@@ -1,25 +1,81 @@
 //! Algorithms by Bansal et al.
 
+use bacon_sci::differentiate::second_derivative;
+use bacon_sci::integrate::integrate;
 use nlopt::Algorithm;
 use nlopt::Nlopt;
-use nlopt::ObjFn;
 use nlopt::Target;
+use std::f64::INFINITY;
 
 use crate::problem::{
     ContinuousHomProblem, ContinuousSchedule, Online, OnlineSolution,
 };
 use crate::PRECISION;
 
-pub type Memory = ();
+static STEP_SIZE: f64 = 1e-16;
 
 impl<'a> Online<ContinuousHomProblem<'a>> {
     /// Deterministic Online Algorithm
+    ///
+    /// * `ps` - Sequence of probability distributions over the number of servers for each time t.
     pub fn bansal(
         &self,
-        _: &ContinuousSchedule,
-        _: &Vec<Memory>,
-    ) -> OnlineSolution<f64, Memory> {
-        (0., ())
+        xs: &ContinuousSchedule,
+        ps: &Vec<impl Fn(f64) -> f64>,
+    ) -> OnlineSolution<f64, impl Fn(f64) -> f64> {
+        let t = xs.len() as i32 + 1;
+        let prev_p = if ps.is_empty() {
+            |j: f64| if j == 0. { 1. } else { 0. }
+        } else {
+            ps[ps.len() - 1]
+        };
+
+        // Determine minimizer of `f` with a convex optimization.
+        let objective_function =
+            |xs: &[f64], _: Option<&mut [f64]>, _: &mut ()| -> f64 {
+                (self.p.f)(t, xs[0]).unwrap()
+            };
+        let mut xs = [0.0];
+        let mut opt = Nlopt::new(
+            Algorithm::Bobyqa,
+            1,
+            objective_function,
+            Target::Minimize,
+            (),
+        );
+        opt.set_lower_bound(0.).unwrap();
+        opt.set_upper_bound(self.p.m as f64).unwrap();
+        opt.set_xtol_rel(PRECISION).unwrap();
+        opt.optimize(&mut xs).unwrap();
+        let x_m = xs[0];
+
+        // Determine `x_r` with a convex optimization.
+        let objective_function =
+            |xs: &[f64], _: Option<&mut [f64]>, _: &mut ()| -> f64 { xs[0] };
+        let mut xs = [x_m];
+        let mut opt = Nlopt::new(
+            Algorithm::Bobyqa,
+            1,
+            objective_function,
+            Target::Maximize,
+            (),
+        );
+        opt.set_lower_bound(0.).unwrap();
+        opt.set_upper_bound(self.p.m as f64).unwrap();
+        opt.set_xtol_rel(PRECISION).unwrap();
+        opt.add_equality_constraint(
+            |xs: &[f64], _: Option<&mut [f64]>, _: &mut ()| -> f64 {
+                integrate(x_m, xs[0], |j: f64| {
+                    second_derivative(|j: f64| (self.p.f)(t, j).unwrap(), j, STEP_SIZE)
+                }, PRECISION).unwrap() - integrate(xs[0], INFINITY, |j: f64| prev_p(j), PRECISION).unwrap()
+            },
+            (),
+            PRECISION,
+        ).unwrap();
+        opt.optimize(&mut xs).unwrap();
+        let x_m = xs[0];
+
+        (1., prev_p.unwrap())
     }
 
     /// Memoryless Deterministic Online Algorithm
@@ -28,45 +84,36 @@ impl<'a> Online<ContinuousHomProblem<'a>> {
         xs: &ContinuousSchedule,
         _: &Vec<()>,
     ) -> OnlineSolution<f64, ()> {
+        let t = xs.len() as i32 + 1;
         let prev_x = if xs.is_empty() { 0. } else { xs[xs.len() - 1] };
-        let f = |j: f64| (self.p.f)(xs.len() as i32 + 1, j).unwrap();
-        let constraint =
+
+        // Determine `x` with a convex optimization.
+        let objective_function =
             |xs: &[f64], _: Option<&mut [f64]>, _: &mut ()| -> f64 {
-                (xs[0] - prev_x).abs() - f(xs[0]) / 2.
+                (self.p.f)(t, xs[0]).unwrap()
             };
-        let x = argmin(f, 0., self.p.m as f64, vec![constraint]);
+        let mut xs = [0.0];
+        let mut opt = Nlopt::new(
+            Algorithm::Bobyqa,
+            1,
+            objective_function,
+            Target::Minimize,
+            (),
+        );
+        opt.set_lower_bound(0.).unwrap();
+        opt.set_upper_bound(self.p.m as f64).unwrap();
+        opt.set_xtol_rel(PRECISION).unwrap();
+        opt.add_inequality_constraint(
+            |xs: &[f64], _: Option<&mut [f64]>, _: &mut ()| -> f64 {
+                (xs[0] - prev_x).abs() - (self.p.f)(t, xs[0]).unwrap() / 2.
+            },
+            (),
+            PRECISION,
+        )
+        .unwrap();
+        opt.optimize(&mut xs).unwrap();
+        let x = xs[0];
 
         (x, ())
     }
-}
-
-/// Determines the minimizer of a convex function `f` on the interval `[a, b]` subject to some inequality constraints (`f <= 0`).
-fn argmin(
-    f: impl Fn(f64) -> f64,
-    a: f64,
-    b: f64,
-    inequality_constraints: Vec<impl ObjFn<()>>,
-) -> f64 {
-    let objective_function =
-        |xs: &[f64], _: Option<&mut [f64]>, _: &mut ()| -> f64 { f(xs[0]) };
-
-    let mut xs = [0.0];
-    let mut opt = Nlopt::new(
-        Algorithm::Bobyqa,
-        1,
-        objective_function,
-        Target::Minimize,
-        (),
-    );
-    opt.set_lower_bound(a).unwrap();
-    opt.set_upper_bound(b).unwrap();
-    opt.set_xtol_rel(PRECISION).unwrap();
-
-    for constraint in inequality_constraints {
-        opt.add_inequality_constraint(constraint, (), PRECISION)
-            .unwrap();
-    }
-
-    opt.optimize(&mut xs).unwrap();
-    xs[0]
 }
