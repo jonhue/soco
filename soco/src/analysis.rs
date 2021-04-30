2b713c17d5e4c784404c1d8558ca76ad19d4a00b
Jonas Hübotter <jonas.huebotter@gmail.com>
2021-04-30 07:38:13 +0200

    fmt

--- soco/src/analysis.rs
+++ soco/src/analysis.rs
@@ -6,7 +6,10 @@ use crate::schedule::Schedule;
 
 use private::Objective;
 
-impl<'a, T> HomProblem<'a, T> where HomProblem<'a, T>: Objective<T> {
+impl<'a, T> HomProblem<'a, T>
+where
+    HomProblem<'a, T>: Objective<T>,
+{
     /// Objective Function. Calculates the cost of a schedule.
     pub fn objective_function(&self, xs: &Schedule<T>) -> Result<f64> {
         self._objective_function(xs, false)
@@ -14,10 +17,7 @@ impl<'a, T> HomProblem<'a, T> where HomProblem<'a, T>: Objective<T> {
 
     /// Inverted Objective Function. Calculates the cost of a schedule. Pays the
     /// switching cost for powering down rather than powering up.
-    pub fn inverted_objective_function(
-        &self,
-        xs: &Schedule<T>,
-    ) -> Result<f64> {
+    pub fn inverted_objective_function(&self, xs: &Schedule<T>) -> Result<f64> {
         self._objective_function(xs, true)
     }
 }
