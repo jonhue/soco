9a842bc02dfc99cc01cac444577388a298c69cf2
Jonas Hübotter <jonas.huebotter@gmail.com>
2021-05-02 18:17:15 +0200

    [#92] Forward Optimal Continuous Offline Algorithm

--- soco/src/algorithms/offline/mod.rs
+++ soco/src/algorithms/offline/mod.rs
@@ -1,4 +1,4 @@
 //! Optimal Offline Algorithms
 
 pub mod iopt;
-pub mod opt_backward;
+pub mod opt;
---
+++ soco/tests/algorithms/offline/opt.rs
@@ -0,0 +1,55 @@
+#![allow(clippy::float_cmp)]
+
+mod opt_backward {
+    use std::sync::Arc;
+
+    use soco::algorithms::offline::opt::opt_backward;
+    use soco::convert::DiscretizableSchedule;
+    use soco::problem::HomProblem;
+    use soco::verifiers::VerifiableSchedule;
+
+    #[test]
+    fn _1() {
+        let p = HomProblem {
+            m: 2,
+            t_end: 2,
+            f: Arc::new(|t, j| {
+                Some(t as f64 * (if j == 0. { 1. } else { 0. }))
+            }),
+            beta: 1.,
+        };
+        p.verify().unwrap();
+
+        let result = opt_backward(&p).unwrap();
+        result.verify(p.m, p.t_end).unwrap();
+
+        assert_eq!(result.to_i(), vec![1, 1]);
+    }
+}
+
+mod opt_forward {
+    use std::sync::Arc;
+
+    use soco::algorithms::offline::opt::opt_forward;
+    use soco::convert::DiscretizableSchedule;
+    use soco::problem::HomProblem;
+    use soco::verifiers::VerifiableSchedule;
+
+    #[test]
+    fn _1() {
+        let p = HomProblem {
+            m: 2,
+            t_end: 2,
+            f: Arc::new(|t, j| {
+                Some(t as f64 * (if j == 0. { 1. } else { 0. }))
+            }),
+            beta: 1.,
+        };
+        p.verify().unwrap();
+
+        let result = opt_forward(&p).unwrap();
+        result.verify(p.m, p.t_end).unwrap();
+
+        assert_eq!(result.to_i(), vec![1, 1]);
+    }
+}
