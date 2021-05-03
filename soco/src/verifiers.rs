e38a46d87e38078f5c5b81bd06e1316044547f64
Jonas Hübotter <jonas.huebotter@gmail.com>
2021-05-03 14:29:58 +0200

    clippy

--- soco/src/verifiers.rs
+++ soco/src/verifiers.rs
@@ -13,37 +13,40 @@ pub trait VerifiableCostFn<'a, T> {
     fn verify(&self, t: i32, x: &T) -> Result<()>;
 }
 
-impl<'a, T> VerifiableCostFn<'a, T> for CostFn<'a, T> {
+impl<'a, T> VerifiableCostFn<'a, T> for CostFn<'a, T>
+where
+    T: std::fmt::Debug,
+{
     fn verify(&self, t: i32, x: &T) -> Result<()> {
         assert_validity(
             self(t, x).ok_or_else(|| {
-                invalid(format!("cost function must be total on its domain"))
+                invalid(format!("cost function must be total on its domain, but returns None for ({}, {:?})", t, x))
             })? >= 0.,
-            format!("cost function must be non-negative"),
+            format!("cost function must be non-negative, but is for ({}, {:?})", t, x),
         )
     }
 }
 
 impl<'a, T> Problem<'a, T>
 where
-    T: Clone + NumCast + PartialOrd,
+    T: Clone + NumCast + PartialOrd + std::fmt::Debug,
 {
     pub fn verify(&self) -> Result<()> {
         assert_validity(
             self.d > 0,
-            format!("number of dimensions must be positive"),
+            format!("number of dimensions must be positive, is {}", self.d),
         )?;
         assert_validity(
             self.t_end > 0,
-            format!("time horizon must be positive"),
+            format!("time horizon must be positive, is {}", self.t_end),
         )?;
         assert_validity(
             self.bounds.len() == self.d as usize,
-            format!("length of vector of upper bounds must equal dimension"),
+            format!("length of vector of upper bounds must equal dimension, {} != {}", self.bounds.len(), self.d),
         )?;
         assert_validity(
             self.betas.len() == self.d as usize,
-            format!("length of vector of switching cost factors must equal dimension"),
+            format!("length of vector of switching cost factors must equal dimension, {} != {}", self.betas.len(), self.d),
         )?;
 
         for k in 0..self.d as usize {
@@ -74,10 +77,13 @@ where
 
 impl<'a, T> Online<Problem<'a, T>>
 where
-    T: Clone + NumCast + PartialOrd,
+    T: Clone + NumCast + PartialOrd + std::fmt::Debug,
 {
     pub fn verify(&self) -> Result<()> {
-        assert_validity(self.w >= 0, format!("w must be non-negative"))?;
+        assert_validity(
+            self.w >= 0,
+            format!("w must be non-negative, is {}", self.w),
+        )?;
 
         self.p.verify()
     }
@@ -94,17 +100,17 @@ where
     fn verify(&self, t_end: i32, bounds: &Vec<T>) -> Result<()> {
         assert_validity(
             self.len() == t_end as usize,
-            format!("schedule must have a value for each time step"),
+            format!("schedule must have a value for each time step, `t_end` is {} and schedule contains {} steps", t_end, self.len()),
         )?;
 
-        for t in 0..self.len() {
-            for k in 0..bounds.len() {
+        for (t, x) in self.iter().enumerate() {
+            for (k, &j) in x.iter().enumerate() {
                 assert_validity(
-                    self[t][k] >= NumCast::from(0).unwrap(),
+                    j >= NumCast::from(0).unwrap(),
                     format!("value at time {} for dimension {} must be non-negative", t + 1, k + 1),
                 )?;
                 assert_validity(
-                    self[t][k] <= bounds[k],
+                    j <= bounds[k],
                     format!("value at time {} for dimension {} must not exceed its upper bound", t + 1, k + 1),
                 )?;
             }
