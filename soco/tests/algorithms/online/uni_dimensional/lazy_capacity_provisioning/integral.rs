8e42c778105b94b1ac833c349536f3fbc7246fd0
Jonas Hübotter <jonas.huebotter@gmail.com>
2021-05-14 13:24:23 +0200

    expand tests

--- soco/tests/algorithms/online/uni_dimensional/lazy_capacity_provisioning/integral.rs
+++ soco/tests/algorithms/online/uni_dimensional/lazy_capacity_provisioning/integral.rs
@@ -10,6 +10,34 @@ mod lcp {
 
     #[test]
     fn _1() {
+        let p = SmoothedConvexOptimization {
+            d: 1,
+            t_end: 1,
+            bounds: vec![2],
+            switching_cost: vec![1.],
+            hitting_cost: Arc::new(|t, j| {
+                Some(t as f64 * (if j[0] == 0 { 1. } else { 0. }))
+            }),
+        };
+        let mut o = Online { p, w: 0 };
+        o.verify().unwrap();
+
+        let result = o
+            .stream(
+                lcp,
+                |_, _, _| false,
+                &Options {
+                    use_approx: None,
+                },
+            )
+            .unwrap();
+        result.0.verify(o.p.t_end, &o.p.bounds).unwrap();
+
+        assert_eq!(result.0, Schedule::new(vec![Config::single(0)]));
+    }
+
+    #[test]
+    fn _2() {
         let approx_options = ApproxOptions { gamma: None };
         let p = SmoothedConvexOptimization {
             d: 1,
@@ -38,7 +66,39 @@ mod lcp {
     }
 
     #[test]
-    fn _2() {
+    fn _3() {
+        let p = SmoothedConvexOptimization {
+            d: 1,
+            t_end: 1,
+            bounds: vec![5],
+            switching_cost: vec![1.],
+            hitting_cost: Arc::new(|t, j| {
+                Some(t as f64 * (if j[0] == 0 { 1. } else { 0. }))
+            }),
+        };
+        let mut o = Online { p, w: 0 };
+        o.verify().unwrap();
+
+        let t_end = 2;
+        let result = o
+            .offline_stream(
+                lcp,
+                t_end,
+                &Options {
+                    use_approx: None,
+                },
+            )
+            .unwrap();
+        result.0.verify(t_end, &o.p.bounds).unwrap();
+
+        assert_eq!(
+            result.0,
+            Schedule::new(vec![Config::single(0), Config::single(1)])
+        );
+    }
+
+    #[test]
+    fn _4() {
         let approx_options = ApproxOptions { gamma: None };
         let p = SmoothedConvexOptimization {
             d: 1,
