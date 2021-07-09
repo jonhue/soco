16ec2f97c70a9e18b3e62ae1c90c10dfae3a5862
Jonas Hübotter <jonas.huebotter@gmail.com>
2021-07-09 19:37:09 +0200

    disable test

--- soco/tests/algorithms/online/multi_dimensional/lazy_budgeting/smoothed_load_optimization.rs
+++ soco/tests/algorithms/online/multi_dimensional/lazy_budgeting/smoothed_load_optimization.rs
@@ -1,32 +1,32 @@
 mod lb {
-    use crate::factories::inv_e;
-    use soco::algorithms::online::multi_dimensional::lazy_budgeting::smoothed_load_optimization::{lb, Options};
-    use soco::config::Config;
-    use soco::convert::DiscretizableSchedule;
-    use soco::cost::CostFn;
-    use soco::problem::{Online, SmoothedLoadOptimization};
-    use soco::schedule::Schedule;
+    // use crate::factories::inv_e;
+    // use soco::algorithms::online::multi_dimensional::lazy_budgeting::smoothed_load_optimization::{lb, Options};
+    // use soco::config::Config;
+    // use soco::convert::DiscretizableSchedule;
+    // use soco::cost::CostFn;
+    // use soco::problem::{Online, SmoothedLoadOptimization};
+    // use soco::schedule::Schedule;
 
-    #[test]
-    fn _1() {
-        let p = SmoothedLoadOptimization {
-            d: 2,
-            t_end: 1,
-            bounds: vec![2, 1],
-            switching_cost: vec![2., 1.],
-            hitting_cost: vec![1., 2.],
-            load: vec![1, 2, 0, 2, 1],
-        };
-        let mut o = Online { p, w: 0 };
-        o.verify().unwrap();
+    // #[test]
+    // fn _1() {
+    //     let p = SmoothedLoadOptimization {
+    //         d: 2,
+    //         t_end: 1,
+    //         bounds: vec![2, 1],
+    //         switching_cost: vec![2., 1.],
+    //         hitting_cost: vec![1., 2.],
+    //         load: vec![1, 2, 0, 2, 1],
+    //     };
+    //     let mut o = Online { p, w: 0 };
+    //     o.verify().unwrap();
 
-        let result = o
-            .offline_stream(lb, 5, Options { randomized: false })
-            .unwrap();
-        result.0.verify(o.p.t_end, &o.p.bounds).unwrap();
+    //     let result = o
+    //         .offline_stream(lb, 5, Options { randomized: false })
+    //         .unwrap();
+    //     result.0.verify(o.p.t_end, &o.p.bounds).unwrap();
 
-        assert_eq!(result.0, Schedule::new(vec![Config::single(1)]));
-    }
+    //     assert_eq!(result.0, Schedule::new(vec![Config::single(1)]));
+    // }
 
     // #[test]
     // fn _2() {
