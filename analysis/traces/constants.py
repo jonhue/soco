53608b26abaf21ed70fabc4bc0a0db0149d69e31
Jonas Hübotter <jonas.huebotter@gmail.com>
2021-07-11 15:59:08 +0200

    [#230] Trace analysis

---
+++ analysis/traces/constants.py
@@ -0,0 +1,8 @@
+SUBMISSION_TIME = 'submission_time'
+START_TIME = 'start_time'
+END_TIME = 'end_time'
+JOB_TYPE = 'job_type'
+SOURCE = 'source'
+SERVER_TYPE = 'server_type'
+LOCATION = 'location'
+NAMES = [SUBMISSION_TIME, START_TIME, END_TIME, JOB_TYPE, SOURCE, SERVER_TYPE, LOCATION]
