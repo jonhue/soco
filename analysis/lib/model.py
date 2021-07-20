33bdc5142214a74227282a7d6e4596dd967e88b6
Jonas Hübotter <jonas.huebotter@gmail.com>
2021-07-20 13:37:45 +0200

    Python utils for modeling and analysis

---
+++ analysis/lib/model.py
@@ -0,0 +1,14 @@
+from soco.data_center.model import (
+    LinearEnergyConsumptionModel,
+    SimplifiedLinearEnergyConsumptionModel,
+    NonLinearEnergyConsumptionModel,
+    LinearEnergyCostModel,
+    QuotasEnergyCostModel,
+    MinimalDetectableDelayRevenueLossModel,
+    SwitchingCost,
+    Location,
+    ServerType,
+    Source,
+    JobType,
+    DataCenterModel,
+)
