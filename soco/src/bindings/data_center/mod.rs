926dcaae72f67f3e04a370f9d077182c941ec139
Jonas Hübotter <jonas.huebotter@gmail.com>
2021-07-21 09:04:28 +0200

    [#251] Make submodules available as packages (#252)
    * [#251] Make submodules available as packages
    
    * fmt

--- soco/src/bindings/data_center/mod.rs
+++ soco/src/bindings/data_center/mod.rs
@@ -1,3 +1,4 @@
+use super::utils::make_package;
 use pyo3::prelude::*;
 
 mod model;
@@ -8,14 +9,17 @@ mod online;
 pub fn submodule(py: Python, m: &PyModule) -> PyResult<()> {
     let offline = PyModule::new(py, "offline")?;
     offline::submodule(py, offline)?;
+    make_package(py, offline, "soco.data_center.offline");
     m.add_submodule(offline)?;
 
     let online = PyModule::new(py, "online")?;
     online::submodule(py, online)?;
+    make_package(py, online, "soco.data_center.online");
     m.add_submodule(online)?;
 
     let model = PyModule::new(py, "model")?;
     model::submodule(py, model)?;
+    make_package(py, model, "soco.data_center.model");
     m.add_submodule(model)?;
 
     Ok(())
