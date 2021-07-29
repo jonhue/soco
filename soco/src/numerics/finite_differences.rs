f9fee613971e12f91cefcfe9139951000a5f909b
Jonas Hübotter <jonas.huebotter@gmail.com>
2021-07-29 09:44:53 +0200

    cleanup

--- soco/src/numerics/finite_differences.rs
+++ soco/src/numerics/finite_differences.rs
@@ -4,31 +4,35 @@ use crate::numerics::{ApplicablePrecision, TOLERANCE};
 use bacon_sci::differentiate::{
     derivative as derivative_, second_derivative as second_derivative_,
 };
-use log::debug;
+use log::warn;
 use noisy_float::prelude::*;
 
 pub fn derivative(f: impl Fn(f64) -> f64, x: f64) -> N64 {
-    let y = f(x);
-    if y.is_infinite() {
+    if f(x).is_infinite() {
         return n64(f64::INFINITY);
     }
-    let r = derivative_(f, x, TOLERANCE).apply_precision();
-    if r.is_nan() {
-        debug!("x={};f(x)={}", x, y);
+
+    let result = derivative_(f, x, TOLERANCE).apply_precision();
+    if result.is_nan() {
+        warn!(
+            "First-order finite difference returned NaN. Assuming result `0`."
+        );
         return n64(0.);
     }
-    n64(r)
+    n64(result)
 }
 
 pub fn second_derivative(f: impl Fn(f64) -> f64, x: f64) -> N64 {
-    let y = f(x);
-    if y.is_infinite() {
+    if f(x).is_infinite() {
         return n64(f64::INFINITY);
     }
-    let r = second_derivative_(f, x, TOLERANCE.powf(-0.25)).apply_precision();
-    if r.is_nan() {
-        debug!("x={};f(x)={}", x, y);
+    let result =
+        second_derivative_(f, x, TOLERANCE.powf(-0.25)).apply_precision();
+    if result.is_nan() {
+        warn!(
+            "Second-order finite difference returned NaN. Assuming result `0`."
+        );
         return n64(0.);
     }
-    n64(r)
+    n64(result)
 }
